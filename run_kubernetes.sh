b01a864ac7466e4e2fe878edc33a14fed31ef7d4
EC2 Default User <ec2-user@ip-172-31-28-230.us-east-2.compute.internal>
2020-08-15 18:10:25 +0000

    Creating cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="clairelee7522/app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward app 8000:80
+© 2020 GitHub, Inc.
